2f938dfbfe28995cef7c80c2aef9cffac79719d7
selvakumarsai <88017125+selvakumarsai@users.noreply.github.com>
2025-02-04 22:10:45 +1100

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ with st.sidebar:
 # Initialize chat engine with selected model(Deepseek)
 llm_engine = ChatOllama(
     model=selected_model,
-    base_url="http://localhost:8501",
+    base_url="http://localhost:11434",
     streaming=True,
     temperature=0.3
 )
